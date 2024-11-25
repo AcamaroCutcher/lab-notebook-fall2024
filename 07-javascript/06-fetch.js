7142763e681db09894fec2edfa28f37465e755f3
AcamaroCutcher <acamarotech@gmail.com>
2024-11-25 14:14:21 -0800

    lab notebook update

--- 07-javascript/06-fetch.js
+++ 07-javascript/06-fetch.js
@@ -4,20 +4,25 @@ const app = document.querySelector("#books");
 
 const addbookToDOM = (book) => {
   let element = document.createElement("div");
-  let title = document.createElement("div");
-  let author = document.createElement("div");
-  let released = document.createElement("div");
-  let pages = document.createElement("div");
+  let title = document.createElement("h3");
+  let author = document.createElement("p");
+  let released = document.createElement("p");
+  let pages = document.createElement("p");
 
   title.textContent = book.name;
   author.textContent = book.authors[0];
-  released.textContent = book.released;
-  pages.textContent = book.numberOfPages;
+  released.textContent = book.released.substr(0, 4);
+  pages.textContent = `${book.numberOfPages} pages`;
 
   element.append(title);
   element.append(author);
   element.append(released);
   element.append(pages);
+
+  element.style.display = "flex";
+  element.style.flexDirection = "column";
+  element.style.alignItems = "center";
+  element.style.marginTop = "20px";
 
   app.append(element);
 };
